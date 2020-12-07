bf509fc6d6c377795156b227780ed9d1cd6efcc8
kanopeld <968597@gmail.com>
2020-12-07 18:37:28 +0300

    add associate method stub

--- handler_assosiate.go
+++ handler_assosiate.go
@@ -1,1 +1,10 @@
 package socks5
+
+import (
+	"context"
+	"fmt"
+)
+
+func (s *Server) handleAssociate(ctx context.Context, conn conn, req *Request) error {
+	return fmt.Errorf("method not implemented")
+}
